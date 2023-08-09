6ab7f98d0cf997f0108c007d418a0113be898b67
Rashaad Lee Hue <rashaadleehue@qwe.local>
2023-08-09 14:38:43 -0400

    eb-update

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -53,7 +53,11 @@ public class PlayerMovement : MonoBehaviour
             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
         }
 
-        UpdateAnimationState();
+        if (!PauseMenu.isPaused)
+        {
+            UpdateAnimationState();
+        }
+            
     }
 
     private void UpdateAnimationState()
